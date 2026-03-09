08b1727854baa13d108d1a5227a27b522314db76
Your Name <aravindsivalasetty.381@gmail.com>
2026-03-09 16:54:04 -0500

    changes

--- shell-pratice/04-variable.sh
+++ shell-pratice/04-variable.sh
@@ -4,7 +4,7 @@
 person1=$1
 person2=$2
 
-echo "bo$person1 :: hello $person2, how are you?"
+echo "$person1 :: hello $person2, how are you?"
 echo "$person2:: i am fine"
 echo "$person1:: good $person2"
 echo "$person2:: ok fine"
