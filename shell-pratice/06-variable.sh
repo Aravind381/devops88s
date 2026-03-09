4920886edaa494e1edb1618fac680401aacc817e
Your Name <aravindsivalasetty.381@gmail.com>
2026-03-09 17:06:14 -0500

    changes

--- shell-pratice/06-variable.sh
+++ shell-pratice/06-variable.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+STARTTIME=$(date +%5)
 
-echo "script exectued at: $TIMESTAMP"
+echo "script exectued at: $STARTTIME"
+
+sleep 10
+
+END_TIME=$(date +%5)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "Script executed in:$TOTAL_TIME"
