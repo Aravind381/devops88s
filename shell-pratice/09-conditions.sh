b30fbe0cf9a4bca03036981a92c23ca5b55a0522
Your Name <aravindsivalasetty.381@gmail.com>
2026-03-09 20:40:04 -0500

    changes

--- shell-pratice/09-conditions.sh
+++ shell-pratice/09-conditions.sh
@@ -9,7 +9,7 @@ NUMBER=25
 if [ $NUMBER -gt 20 ]; then
     echo "Given number: $NUMBER is grater than 20"
 elif [ $NUMBER -eq 20 ]; then
-    echo:Given number: $NUMBER is equal to 20"
+    echo "Given number: $NUMBER is equal to 20"
 
 else
     echo "given numer: $NUMBER is less than 20"    
