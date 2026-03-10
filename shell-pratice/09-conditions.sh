ab20cee698549298218f779852455725415b6f6b
Your Name <aravindsivalasetty.381@gmail.com>
2026-03-09 20:39:08 -0500

    changes

--- shell-pratice/09-conditions.sh
+++ shell-pratice/09-conditions.sh
@@ -8,6 +8,8 @@ NUMBER=25
 # -ne -> not equal
 if [ $NUMBER -gt 20 ]; then
     echo "Given number: $NUMBER is grater than 20"
+elif [ $NUMBER -eq 20 ]; then
+    echo:Given number: $NUMBER is equal to 20"
 
 else
     echo "given numer: $NUMBER is less than 20"    
