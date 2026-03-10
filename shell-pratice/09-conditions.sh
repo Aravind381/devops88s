cb4f674723479f8ad0d65514efcfd8e003481cc4
Your Name <aravindsivalasetty.381@gmail.com>
2026-03-09 20:35:56 -0500

    changes

---
+++ shell-pratice/09-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=25
+
+# -gt  -> grater than
+#-lt -> less than
+# -eq -> equal
+# -ne -> not equal
+if [ $NUMBER -gt 20 ]; then
+    echo "Given number: $NUMBER is grater than 20"
+fi    
