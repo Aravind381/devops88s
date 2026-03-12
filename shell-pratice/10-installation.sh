9457d85333e600afead52e507a5e1314495bae52
Your Name <aravindsivalasetty.381@gmail.com>
2026-03-12 17:09:05 -0500

    changes

--- shell-pratice/10-installation.sh
+++ shell-pratice/10-installation.sh
@@ -9,3 +9,28 @@ fi
 
 echo "Instally Nginx"
 dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing nginx ... Failure"
+    exit 1
+else
+    echo "Installing nginx ... success"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing MYSQL ... Failure"
+    exit 1
+else
+    echo "Installing MYSQL ... success"
+fi        
+
+dnf install nodejs -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing nodejs ... Failure"
+    exit 1
+else
+    echo "Installing nodejs ... success"
+fi        
